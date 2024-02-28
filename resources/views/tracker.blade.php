f0243077011f4fa0046696349390b06b0f14c345
Elivince <elivincemateos.timbas@bicol-u.edu.ph>
2024-02-28 13:48:26 +0800

    Cleaned code

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -32,7 +32,6 @@
                         </thead>
                         <tbody>
                             @foreach ($requests as $request)
-                            <!-- Add your data here -->
                             <tr>
                                 <td class="px-6 py-2 text-center whitespace-no-wrap border-b border-gray-200 dark:border-gray-600">{{ $request->date }}</td>
                                 <td class="px-6 py-2 text-center whitespace-no-wrap border-b border-gray-200 dark:border-gray-600">{{ $request->name }}</td>
