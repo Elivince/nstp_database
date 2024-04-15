107b3b92845c38eb34299188341bb148c07b0ebc
Elivince <elivincemateos.timbas@bicol-u.edu.ph>
2024-04-15 16:37:13 +0800

    removed unnecessary spaces

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -295,7 +295,6 @@
                 </div>
             </div>
 
-
             <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
                 <span class="flex items-center col-span-3"> Showing {{ $requests->firstItem() }} - {{ $requests->lastItem() }} of {{ $requests->total() }} </span>
                 <span class="col-span-2"></span>
