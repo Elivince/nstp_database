fb8a8d770a5be1ae186f689ab4787b0a93098b8a
Gerryminah Aguilar <gerryminah04aguilar@gmail.com>
2024-04-18 10:09:57 +0800

    dashboard initial

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -357,116 +357,7 @@
 
 
                  <!-- Task Summaries -->
-                 <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 p-4 gap-4 text-black dark:text-white">
-                     <div class="md:col-span-2 xl:col-span-3">
-                         <h3 class="text-lg font-semibold">Task Summaries and Activities</h3>
-                     </div>
-                     <div class="md:col-span-2 xl:col-span-1">
-                         <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
-                             <div class="flex justify-between py-1 text-black dark:text-white">
-                                 <h3 class="text-sm font-semibold">TO DO List</h3>
-                                 <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                     <path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" />
-                                 </svg>
-                             </div>
-                             <div class="text-sm text-black dark:text-gray-50 mt-2">
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Delete all references from the wiki</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Remove analytics code</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">
-                                     Do a mobile first layout
-                                     <div class="text-gray-500 dark:text-gray-200 mt-2 ml-2 flex justify-between items-start">
-                                         <span class="text-xs flex items-center">
-                                             <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                                                 <path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z" />
-                                             </svg>
-                                             3/5
-                                         </span>
-                                         <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
-                                     </div>
-                                 </div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Check the meta tags</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">
-                                     Think more tasks for this example
-                                     <div class="text-gray-500 dark:text-gray-200 mt-2 ml-2 flex justify-between items-start">
-                                         <span class="text-xs flex items-center">
-                                             <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                                                 <path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z" />
-                                             </svg>
-                                             0/3
-                                         </span>
-                                     </div>
-                                 </div>
-                                 <p class="mt-3 text-gray-600 dark:text-gray-400">Add a card...</p>
-                             </div>
-                         </div>
-                     </div>
-                     <div>
-                         <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
-                             <div class="flex justify-between py-1 text-black dark:text-white">
-                                 <h3 class="text-sm font-semibold">Activities</h3>
-                                 <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                     <path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" />
-                                 </svg>
-                             </div>
-                             <div class="text-sm text-black dark:text-gray-50 mt-2">
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Delete all references from the wiki</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Remove analytics code</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">
-                                     Do a mobile first layout
-                                     <div class="flex justify-between items-start mt-2 ml-2 text-white text-xs">
-                                         <span class="bg-pink-600 rounded p-1 text-xs flex items-center">
-                                             <svg class="h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                 <path d="M12 2c-.8 0-1.5.7-1.5 1.5v.688C7.344 4.87 5 7.62 5 11v4.5l-2 2.313V19h18v-1.188L19 15.5V11c0-3.379-2.344-6.129-5.5-6.813V3.5c0-.8-.7-1.5-1.5-1.5zm-2 18c0 1.102.898 2 2 2 1.102 0 2-.898 2-2z" />
-                                             </svg>
-                                             2
-                                         </span>
-                                     </div>
-                                 </div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Check the meta tags</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">
-                                     Think more tasks for this example
-                                     <div class="text-gray-500 mt-2 ml-2 flex justify-between items-start">
-                                         <span class="text-xs flex items-center">
-                                             <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                                                 <path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z" />
-                                             </svg>
-                                             0/3
-                                         </span>
-                                     </div>
-                                 </div>
-                                 <p class="mt-3 text-gray-600 dark:text-gray-400">Add a card...</p>
-                             </div>
-                         </div>
-                     </div>
-                     <div>
-                         <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
-                             <div class="flex justify-between py-1 text-black dark:text-white">
-                                 <h3 class="text-sm font-semibold">Notes</h3>
-                                 <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                     <path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" />
-                                 </svg>
-                             </div>
-                             <div class="text-sm text-black dark:text-gray-50 mt-2">
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Delete all references from the wiki</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Remove analytics code</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Do a mobile first layout</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">Check the meta tags</div>
-                                 <div class="bg-white dark:bg-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700 p-2 rounded mt-1 border-b border-gray-100 dark:border-gray-900 cursor-pointer">
-                                     Think more tasks for this example
-                                     <div class="text-gray-500 dark:text-gray-200 mt-2 ml-2 flex justify-between items-start">
-                                         <span class="text-xs flex items-center">
-                                             <svg class="h-4 fill-current mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
-                                                 <path d="M11 4c-3.855 0-7 3.145-7 7v28c0 3.855 3.145 7 7 7h28c3.855 0 7-3.145 7-7V11c0-3.855-3.145-7-7-7zm0 2h28c2.773 0 5 2.227 5 5v28c0 2.773-2.227 5-5 5H11c-2.773 0-5-2.227-5-5V11c0-2.773 2.227-5 5-5zm25.234 9.832l-13.32 15.723-8.133-7.586-1.363 1.465 9.664 9.015 14.684-17.324z" />
-                                             </svg>
-                                             0/3
-                                         </span>
-                                     </div>
-                                 </div>
-                                 <p class="mt-3 text-gray-600 dark:text-gray-400">Add a card...</p>
-                             </div>
-                         </div>
-                     </div>
-                 </div>
+
                  <!-- ./Task Summaries -->
 
                  <!-- Contact Form -->
@@ -486,8 +377,8 @@
                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                                  </svg>
                                  <div class="container">
-                                     <div class="ml-4 text-md tracking-wide font-semibold w-80">BU Student Union Center</div>
-                                     <div class="ml-4 text-sm tracking-wide font-semibold w-30">Legazpi City, Albay</div>
+                                     <div class="ml-4 text-md tracking-wide font-semibold w-80">3rd Floor, Student Union Center Building, Bicol University, Legazpi City</div>
+
                                  </div>
                              </div>
 
@@ -495,7 +386,7 @@
                                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
                                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                  </svg>
-                                 <div class="ml-4 text-md tracking-wide font-semibold w-40">+6390000000</div>
+                                 <div class="ml-4 text-md tracking-wide font-semibold w-40 "><a href="https://www.facebook.com/BicolUNSTPOffice">https://www.facebook.com/BicolUNSTPOffice</a></div>
                              </div>
 
                              <div class="flex items-center mt-4 text-gray-600 dark:text-gray-400">
