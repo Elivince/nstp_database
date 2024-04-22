5376000ab541a11773547d61e693d5798d858e99
Gerryminah Aguilar <gerryminah04aguilar@gmail.com>
2024-04-22 13:46:47 +0800

    erase notification

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -40,17 +40,7 @@
                                  <span class="ml-2 text-sm tracking-wide truncate">Messages</span>
                              </a>
                          </li> -->
-                <li>
-                    <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-blue-800 dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
-                        <span class="inline-flex justify-center items-center ml-4">
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-                            </svg>
-                        </span>
-                        <span class="ml-2 text-sm tracking-wide truncate">Notifications</span>
-                        <span class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">1.2k</span>
-                    </a>
-                </li>
+
                 <li class="px-5 hidden md:block">
                     <div class="flex flex-row items-center mt-5 h-8">
                         <div class="text-sm font-light tracking-wide text-gray-400 uppercase">Settings</div>
@@ -166,48 +156,48 @@
                     </div>
 
                     <form id="addEventToCalendar" action="/addEvent" method="POST">
-                    <div class="shadow rounded-lg bg-white overflow-hidden w-full block p-8">
-                        <h2 class="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">
-                            Add Event Details
-                        </h2>
+                        <div class="shadow rounded-lg bg-white overflow-hidden w-full block p-8">
+                            <h2 class="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">
+                                Add Event Details
+                            </h2>
 
                             @csrf
                             <div class="mb-4">
                                 <label for="event_title" class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Event Details</label>
-                                <input type="text" name="event_title" id="event_title" x-model="event_title" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"/>
+                                <input type="text" name="event_title" id="event_title" x-model="event_title" class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
                             </div>
 
                             <div class="mb-4">
                                 <label for="event_date" class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Event Date</label>
-                                <input type="text" name="event_date" id="event_date" x-model="event_date" readonly  class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"/>
+                                <input type="text" name="event_date" id="event_date" x-model="event_date" readonly class="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
                             </div>
 
                             <div class="inline-block w-64 mb-4">
 
                                 <label for="event_theme" class="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Select a theme</label>
-                            <div class="relative">
-                                <select id="event_theme" @change="event_theme = $event.target.value;" x-model="event_theme" class="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700">
-                                    <template x-for="(theme, index) in themes">
-                                        <option :value="theme.value" x-text="theme.label"></option>
-                                    </template>
-                                </select>
-                                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                    <!-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <div class="relative">
+                                    <select id="event_theme" @change="event_theme = $event.target.value;" x-model="event_theme" class="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700">
+                                        <template x-for="(theme, index) in themes">
+                                            <option :value="theme.value" x-text="theme.label"></option>
+                                        </template>
+                                    </select>
+                                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                                        <!-- <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                                     </svg> -->
+                                    </div>
                                 </div>
                             </div>
+                            <div class="mt-8 text-right">
+                                <button type="button" class="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-300 rounded-lg shadow-sm mr-2" @click="openEventModal = !openEventModal">
+                                    Cancel
+                                </button>
+                                <button type="button" class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-700 rounded-lg shadow-sm" @click="addEvent()">
+                                    Save Event
+                                </button>
+                            </div>
+
                         </div>
-                        <div class="mt-8 text-right">
-                            <button type="button" class="bg-white hover:bg-gray-100 text-gray-700 font-semibold py-2 px-4 border border-gray-300 rounded-lg shadow-sm mr-2" @click="openEventModal = !openEventModal">
-                                Cancel
-                            </button>
-                            <button type="button" class="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-700 rounded-lg shadow-sm" @click="addEvent()">
-                                Save Event
-                            </button>
-                        </div>
-                        
-                    </div>
                     </form>
                 </div>
             </div>
