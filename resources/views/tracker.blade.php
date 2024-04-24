938901a56de327271577426a39c74e802f9ff8a8
Gerryminah Aguilar <gerryminah04aguilar@gmail.com>
2024-04-24 15:22:46 +0800

    fix pagination

--- resources/views/tracker.blade.php
+++ resources/views/tracker.blade.php
@@ -298,8 +298,9 @@
             <div class="grid px-4 py-3 text-xs font-semibold tracking-wide text-gray-500 uppercase border-t dark:border-gray-700 bg-gray-50 sm:grid-cols-9 dark:text-gray-400 dark:bg-gray-800">
                 <span class="flex items-center col-span-3"> Showing {{ $requests->firstItem() }} - {{ $requests->lastItem() }} of {{ $requests->total() }} </span>
                 <span class="col-span-2"></span>
+                <!-- <span class="col-span-2"></span> -->
                 <!-- Pagination -->
-                <div class="flex justify-center mt-6">
+                <div class="flex items-end justify-end col-span-4 mr-4">
                     {{ $requests->links() }}
                 </div>
             </div>
